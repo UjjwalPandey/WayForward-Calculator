5a5b97c40d2297bc5265cf633b34f0803975a5bb
Ujjwal Pandey <ujwalpande@gmail.com>
2020-06-03 18:12:36 +0530

    Files upload

--- script.js
+++ script.js
@@ -87,7 +87,6 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-    // const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -112,10 +111,6 @@ equalsButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
-// allClearButton.addEventListener('click', button => {
-//     calculator.clear()
-//     calculator.updateDisplay()
-// })
 
 deleteButton.addEventListener('click', button => {
     calculator.delete()
